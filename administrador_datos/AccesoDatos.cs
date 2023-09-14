c929cf99778aec7bded832c4bcba13f30e5127db
HoracioBarral <horacio.barral@hotmail.com>
2023-09-14 14:57:08 -0300

    Incorporacion de la ventana de listado de articulos

--- administrador_datos/AccesoDatos.cs
+++ administrador_datos/AccesoDatos.cs
@@ -22,7 +22,7 @@ namespace administrador_datos
 
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server=DESKTOP-8GKVPS1\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
             comando= new SqlCommand();
         }
 
---
+++ administrador_datos/ListaMarcas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using moldes_clases;
+
+namespace administrador_datos
+{
+    public class ListaMarcas
+    {
+        public void Listar()
+        {
+            List<Marca> listadoMarcas = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.SetConsulta("select id,Descripcion from marcas");
+
+            datos.Consulta_A_DB();
+            while (datos.Lector != null)
+            {
+                Marca marca = new Marca();
+                marca.Id = (int)datos.Lector["id"];
+                marca.Descripcion = (string)datos.Lector["Descripcion"];
+            }
+        }
+
+    }
+}
